f0df96741a66a8f4b7230bb3083ea4d2cf205f2f
Vinicius Rufino <vinicius.rufino@riachuelo.com.br>
2020-08-22 17:17:01 -0300

    Criaçao de models, config, controller e routes para exemplo com entidade Tutorial

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./app/models');
+const app = express();
+
+const corsOptions ={
+  origin: 'http://localhost:8081'
+}
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+app.get('/', (req, res) => {
+  res.json({message: 'Welcome to Node-Rest-Database App'});
+});
+
+require("./app/routes/tutorial.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
