0fe449fa2ca52c91c38499856ef5ce271ac3c7d5
Vinicius Rufino <vinicius.rufino@riachuelo.com.br>
2020-08-22 20:39:07 -0300

    correçao de porta no cors e adiçao de gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./app/models');
 const app = express();
 
 const corsOptions ={
-  origin: 'http://localhost:8081'
+  origin: 'http://localhost:3000'
 }
 
 app.use(cors(corsOptions));
